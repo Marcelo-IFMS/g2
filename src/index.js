3c53a3bb0a682ced74f0d888f372d55236da9aa4
Marcelo <marcelo.perez@ifms.edu.br>
2025-11-18 11:10:46 -0400

    mongo ejs consign route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // Use "type: commonjs" in package.json to use CommonJS modules
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+const app = require('./config/server')
 const port = 3000;
  
 // Define your routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Hello from Express on Vercel!' });
+// });
  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
